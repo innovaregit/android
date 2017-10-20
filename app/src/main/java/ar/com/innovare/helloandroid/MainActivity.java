7cf14d0abc2d36f7ff5eb9c4dfc07eaa2e0e8c91
Marcelo Agustini <agustinimarcelo@gmail.com>
2017-10-20 20:41:25 -0300

    SUMA

--- app/src/main/java/ar/com/innovare/helloandroid/MainActivity.java
+++ app/src/main/java/ar/com/innovare/helloandroid/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mensaje;
     Button boton;
+    EditText numero1;
+    EditText numero2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +22,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mensaje = (TextView) findViewById(R.id.mensaje);
         boton= (Button) findViewById(R.id.boton);
+        numero1 = (EditText) findViewById(R.id.numero1);
+        numero2 = (EditText) this.findViewById(R.id.numero2);  //IGUAL
 
         boton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                mensaje.setText("Hola Android!!!");
+                Integer num1= Integer.valueOf(numero1.getText().toString()); // Objeto
+                Integer num2= Integer.valueOf(numero2.getText().toString());
+                Integer suma = num1+num2;
+                mensaje.setText("La suma es de " + suma.toString());
             }
         });
 
